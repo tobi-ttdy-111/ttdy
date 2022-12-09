01d7c4879921b165a252a0cc64ab863c8ab7e835
tobi-ttdy-111 <104658114+tobi-ttdy-111@users.noreply.github.com>
2022-12-09 16:52:16 -0600

    Update programa7.c

--- programa7/programa7.c
+++ programa7/programa7.c
@@ -1,34 +1,41 @@
-
-// includes
-#include <stdio.h>
-#include <stdlib.h>
+# include <stdio.h>
+# include <stdlib.h>
 
 
-// main
-void main() {
+int main(){
 
-    int longitud;
-    printf( "\nLongitud de la matriz n x m: " );
-    scanf( "%i", &longitud );
+    int filas  = 0, colmunas = 0;
+    char ** p, caracter = '*';
+    char someName;
 
-    int numero;
-    printf( "Numero de relleno: " );
-    scanf( "%i", &numero );
+    printf( "\nNumero filas y columnas: ");
+    scanf( "%i", &filas );
+    colmunas = filas ;
+    printf("\nCaracter de llenado: ");
+    scanf( "%s", &caracter );
 
-    int arreglo[ longitud ][ longitud ];
-    for ( int i = 0; i < longitud; i++ ) {
-        for ( int j = 0; j < longitud; j++ ) {
-            arreglo[ i ][ j ] = numero;
-        };
-        arreglo[ i ][ i ] = i;
-    };
-
-    for ( int i = 0; i < longitud; i++ ) {
-        printf( "\n" );
-        for ( int j = 0; j < longitud; j++ ) {
-            printf( "%i   ", arreglo[ i ][ j ] );
+    p = malloc( filas * ( sizeof( char* ) ) );
+    for ( int i = 0; i < filas ; i++ ) {
+        someName = i+'0';
+        * ( p + i ) = malloc( colmunas * ( sizeof( char ) ) );
+        for ( int j = 0; j < colmunas; j++ ) {
+            if ( i == j ) *(*(p+i)+j) = someName;
+            else *( *(p+i)+j ) = caracter;
         };
     };
 
+    for ( int i = 0; i < filas ; i++ ) {
+        printf("[");
+        for (int j = 0; j < colmunas; j++) {
+            printf(" %c ", *(*(p+i)+j));
+        };
+        printf("]\n");
+    };
 
-};
+    for (int i = 0; i < filas ; i++) { 
+        free(*(p+i));
+    };
+
+    return 0;
+
+}
